96b0e005dd2db4b518c0805fb8b4aa57c483ce24
Luiz Claudio <luizclaudioarruda@outlook.com>
2023-03-02 16:53:56 -0300

    Update NLP Engine

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.post('/', async (req, res) => {
     const prompt = req.body.prompt;
 
     const response = await openai.createCompletion({
-      model: "text-davinci-003",
+      model: "gpt-3.5-turbo",
       prompt: `Seu nome é Aurora. Finja que você é uma IA médica oncologista para responder à perguntas sobre doenças, especialmente câncer. Fale em português do brasil sempre.${prompt}`,
       temperature: 0, // Higher values means the model will take more risks.
       max_tokens: 500, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
