7d1ff8e3a9afddab88feb86f55f6f433474e57fd
Luiz Claudio <luizclaudioarruda@outlook.com>
2023-03-10 15:49:09 -0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.post('/', async (req, res) => {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: `Her name is Aurora. Pretend you are an oncologist medical AI Assistant to answer questions about diseases, especially cancer. Answer in Brazilian Portuguese.${prompt}`,
-      temperature: 0, // Higher values means the model will take more risks.
+      temperature: 0.8, // Higher values means the model will take more risks.
       max_tokens: 500, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
       frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
